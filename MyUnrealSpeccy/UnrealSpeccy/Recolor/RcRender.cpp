4c4ecde55c5332fef444223f2f507ed5424d67e3
Maxim Mozgovoy <rgsoftwarepub@gmail.com>
2019-07-28 22:48:32 +0900

    obsolete code removed

--- MyUnrealSpeccy/UnrealSpeccy/Recolor/RcRender.cpp
+++ MyUnrealSpeccy/UnrealSpeccy/Recolor/RcRender.cpp
@@ -1,6 +1,5 @@
 
 /* (begin) ready screen  -- for debug */
-
 /*void create_my_line(unsigned char *dst, unsigned char *src, unsigned *tab, bool isbw)
 {
 	unsigned ink = 0x00000000, paper = 0x00FFFFFF;
@@ -28,44 +27,39 @@ void create_my_screen(unsigned char *dst, unsigned pitch, bool isbw)
 }*/
 /* (end) ready screen  -- for debug */
 
-void create_my_zxline(unsigned char *dst, unsigned char *src, const unsigned *tab)
+void create_zxline(unsigned char *dst, unsigned char *src, const unsigned *tab)
 {
 	for (unsigned x = 0; x < 320 / 8; ++x)
 	{
-		unsigned char byte = *src++; // a bit sequence of 8 pixels
-		const unsigned *tt = tab + *src++; // attributes
+		unsigned char byte = *src++;		// a bit sequence of 8 pixels
+		const unsigned *tt = tab + *src++;	// attributes (ignore them here)
 
 		*(dst + x) = byte;
 	}
 }
 
-void create_my_zxscreen(unsigned char *src, unsigned char* dst, unsigned delta, const unsigned *tab)
+void create_zxscreen(unsigned char *src, unsigned char* dst, unsigned delta, const unsigned *tab)
 {
 	for (unsigned y = 0; y < 240; y++)
 	{
-		create_my_zxline(dst, src, tab);
+		create_zxline(dst, src, tab);
 		dst += 320 / 8;
 		src += delta;
 	}
 }
 
-//unsigned char color_screen[320*240*4];
-//unsigned char bw_screen[320*240*4];
-
-unsigned char zx_screen[240 * 320 / 8];
-
-//void render_custom(unsigned char *dst, unsigned pitch, unsigned char* zx_screen, unsigned char* color_screen);
-
-// $RG ..........
-
 void rend_dbl(unsigned char *dst, unsigned pitch);
-void render_custom(unsigned char *dst, unsigned pitch, unsigned char* zx_screen, unsigned char* color_screen);
+void recolor_render_impl(unsigned char *dst, unsigned pitch, unsigned char* zx_screen, unsigned char* color_screen);
 
 void recolor_render(unsigned char *src, unsigned char *dst, unsigned pitch, unsigned delta, const unsigned *tab)
 {
+	// zx screen is just a 2D array of bytes
+	// each byte represents a black/white line of 8 pixels
+	unsigned char zx_screen[240 * 320 / 8];
+	
 	//create_my_screen(dst, pitch, true); // renders bw or color screen (mostly for debug)
-	create_my_zxscreen(src, zx_screen, delta, tab);
+	create_zxscreen(src, zx_screen, delta, tab);
 
 	rend_dbl(dst, pitch); // standard 2D renderer
-	render_custom(dst, pitch, zx_screen, nullptr); // recoloring
+	recolor_render_impl(dst, pitch, zx_screen, nullptr); // recoloring
 }
