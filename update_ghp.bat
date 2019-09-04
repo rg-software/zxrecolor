509e7d987c434efd5ae90b9b1289ef47890797a4
Maxim Mozgovoy <rgsoftwarepub@gmail.com>
2019-09-05 04:40:48 +0900

    update_ghp improved

--- update_ghp.bat
+++ update_ghp.bat
@@ -1,3 +1,7 @@
+if "%1" == "skip" goto skip
+copy /y update_ghp.bat "%TEMP%"\update_ghp.bat 
+"%TEMP%"\update_ghp.bat skip
+:skip
 hg up default
 call make_docs.bat
 del index.html
@@ -5,4 +9,5 @@ hg up gh-pages
 robocopy Docs/ .  /s /move
 hg addremove *
 hg commit -m "Documentation update"
+rem hg bookmark -r gh-pages gh-pages
 hg up default
